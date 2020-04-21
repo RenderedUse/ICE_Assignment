8c4d6c683c7788f7b9724be74950831b17f1eccc
Jacob <jbiase123@gmail.com>
2020-04-20 20:23:17 -0400

    BJPlayer Fix

--- src/ca/sheridancollege/project/BlackJackClass/BJPlayer.java
+++ src/ca/sheridancollege/project/BlackJackClass/BJPlayer.java
@@ -1,38 +1,20 @@
 package ca.sheridancollege.project.BlackJackClass;
 
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
-
 /**
  *
  * @author jbias
  */
 public class BJPlayer extends Player{
-    int sum;
-    private final String id;
+    private int sum;
     BJPlayer(String name){
         id=name;
-        Hand ch = new Hand();
-        
-        //create 52 Cards
-        ch.generateHand();
-        
-        for(Card c: ch.cards)
-        {
-            System.out.println(c.getValue() + " of " + c.getSuit());
-        }
-    }
-    @Override public void play (){
-        
+	Hand ch = new Hand();
     }
     public int getSum(){
         return sum;
     }
     public void getCard(){
-        int value=0;
+        int value=Hand.getCard();
         sum+=value;
     }
 }
